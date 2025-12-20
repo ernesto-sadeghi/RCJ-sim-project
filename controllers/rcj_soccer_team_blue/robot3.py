c5b33823b85bb9b3eb0c4a1c14aa85b4b53ccda4
Ernesto <ernestosadeghi@gmail.com>
2025-12-21 00:57:18 +0330

    basic logic based on Daemi course GK(R1) : stand in goal R2,R3 : follow the  ball new logic : communication between robots

--- controllers/rcj_soccer_team_blue/robot3.py
+++ controllers/rcj_soccer_team_blue/robot3.py
@@ -9,47 +9,22 @@ from rcj_soccer_robot import RCJSoccerRobot, TIME_STEP
 
 
 class MyRobot3(RCJSoccerRobot):
-    def run(self):
-        while self.robot.step(TIME_STEP) != -1:
+   def run(self):
+        state=1
+        utils.definitions(self)
+        while self.robot.step(TIME_STEP) !=-1:
             if self.is_new_data():
-                data = self.get_new_data()  # noqa: F841
+                utils.readData(self)
 
-                while self.is_new_team_data():
-                    team_data = self.get_new_team_data()  # noqa: F841
-                    # Do something with team data
-
-                if self.is_new_ball_data():
-                    ball_data = self.get_new_ball_data()
+                if self.is_ball:
+                    if state == 1:
+                        utils.move_to(self,self.xb,self.yb+0.15)
+                        if abs(self.xb-self.xr) <0.01 and abs((self.yb+0.15)-self.yr) < 0.05 :
+                            state=2
+                    elif state ==2:
+                        utils.move_to(self,self.xb,self.yb)
+                        if abs(self.xb-self.xr) > 0.2 and abs(self.yb-self.yr) > 0.2 :
+                            state=2
                 else:
-                    # If the robot does not see the ball, stop motors
-                    self.left_motor.setVelocity(0)
-                    self.right_motor.setVelocity(0)
-                    continue
 
-                # Get data from compass
-                heading = self.get_compass_heading()  # noqa: F841
-
-                # Get GPS coordinates of the robot
-                robot_pos = self.get_gps_coordinates()  # noqa: F841
-
-                # Get data from sonars
-                sonar_values = self.get_sonar_values()  # noqa: F841
-
-                # Compute the speed for motors
-                direction = utils.get_direction(ball_data["direction"])
-
-                # If the robot has the ball right in front of it, go forward,
-                # rotate otherwise
-                if direction == 0:
-                    left_speed = 7
-                    right_speed = 7
-                else:
-                    left_speed = direction * 4
-                    right_speed = direction * -4
-
-                # Set the speed to motors
-                self.left_motor.setVelocity(left_speed)
-                self.right_motor.setVelocity(right_speed)
-
-                # Send message to team robots
-                self.send_data_to_team(self.player_id)
+                   utils.move_to(self,-0.3,0.2,s=True)
--- controllers/rcj_soccer_team_blue/utils.py
+++ controllers/rcj_soccer_team_blue/utils.py
@@ -1,13 +1,115 @@
-def get_direction(ball_vector: list) -> int:
-    """Get direction to navigate robot to face the ball
 
-    Args:
-        ball_vector (list of floats): Current vector of the ball with respect
-            to the robot.
 
-    Returns:
-        int: 0 = forward, -1 = right, 1 = left
+import math 
+
+
+
+# this able robots to cummunicate
+def definitions(robot):
+    robot.heading=0
+    robot.xr=0
+    robot.yr=0
+    robot.xb=0
+    robot.yb=0
+    robot.is_ball=False
+
+
+def move_to (robot,x,y,s=False):
     """
-    if -0.13 <= ball_vector[1] <= 0.13:
-        return 0
-    return -1 if ball_vector[1] < 0 else 1
+    move to somewhere
+    """
+#  head angle
+    heading = math.degrees(robot.get_compass_heading())
+
+# difference angle of head and ball 
+    a = math.degrees(math.atan2((robot.xr-x),(robot.yr-y)))
+
+
+    e= heading + a
+    if e> 180: e-=360
+    if e< -180: e-=360
+
+
+    if a> -90 and a < 90 :
+        right_speed =10+e*0.3
+        left_speed = 10-e*0.3
+    else:
+        if e> 0 :e -=180
+        else: e += 180
+        right_speed =-10+e*0.3
+        left_speed = -10-e*0.3
+
+    if right_speed > 10 : right_speed = 10    
+    if right_speed < -10 : right_speed = -10
+    if left_speed > 10 : left_speed = 10
+    if left_speed < -10 : left_speed = -10
+
+    if abs(robot.xr-x) <0.01 and abs(robot.yr-y) < 0.01 and s :
+        stop(robot)
+    else:
+            
+        robot.right_motor.setVelocity(right_speed)
+        robot.left_motor.setVelocity(left_speed)
+
+def stop(robot):
+    """
+    robot.right_motor.setVelocity(0)
+    <br>
+    <br>
+    robot.left_motor.setVelocity(0)
+
+    
+    """
+    robot.right_motor.setVelocity(0)
+    robot.left_motor.setVelocity(0)
+
+def readData(robot):
+    """
+    <h2>this function is for robots communication.</h2>
+    <br>
+    <mark>To notfy all robots whenever the ball is detected by any robot.</mark>
+    """
+    # robot exact location
+    gps =robot.get_gps_coordinates()
+
+    robot.heading = math.degrees(robot.get_compass_heading())
+
+    robot.xr = gps[0]
+    robot.yr = gps[1]
+
+    # reverse the x and y axis for yellow team
+    if robot.team =="Y":
+        robot.xr *= -1
+        robot.yr *= -1
+
+    if robot.is_new_ball_data():
+        ball_data = robot.get_new_ball_data()
+        ball_angle = math.degrees(math.atan2(ball_data["direction"][1],ball_data["direction"][0]))
+        ball_distance= abs(0.01666666/(abs(ball_data["direction"][2])/math.sqrt(1-ball_data["direction"][2]**2)))
+
+        robot.xb = math.sin(math.radians(ball_angle+robot.heading))*ball_distance +robot.xr
+        robot.yb = -math.cos(math.radians(ball_angle+robot.heading))*ball_distance +robot.yr
+        
+        robot.is_ball =True
+    else:
+        robot.is_ball =False
+    robot.send_data_to_team({
+        'is_ball':robot.is_ball,
+        'xr':robot.xr,
+        'yr':robot.yr,
+        'xb':robot.xb,
+        'yb':robot.yb,
+        'id':robot.player_id
+    })
+        
+    # To notfy all robots whenever the ball is detected by any robot. 
+    while robot.is_new_team_data():
+
+        # print(robot.get_new_team_data())    //==>  {'robot_id': {'is_ball': True, 'xr': -0.25333272879690055, 'yr': 0.26441205057572287, 'xb': -0.14169967192514168, 'yb': 0.14789680353410498, 'id': 2}}
+
+        team_data = robot.get_new_team_data()['robot_id']
+
+        if not robot.is_ball and team_data["is_ball"]:
+            robot.is_ball =True
+            robot.xb = team_data["xb"]
+            robot.yb = team_data["yb"]
